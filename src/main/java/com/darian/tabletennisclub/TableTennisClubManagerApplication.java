da5506ff446b1f9ed6010f538a7f9a3e2a48a3b5
Darian <Darian.gin@outlook.com>
2026-08-16 14:04:14 +1000

    Initial project setup

---
+++ src/main/java/com/darian/tabletennisclub/TableTennisClubManagerApplication.java
@@ -0,0 +1,13 @@
+package com.darian.tabletennisclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableTennisClubManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableTennisClubManagerApplication.class, args);
+	}
+
+}
